cad536774edf12e494142e731fcc1b4a76387d39
ELynx <esurovin@yandex.ru>
2016-08-13 23:53:34 +0300

    swithc to thread context, hook different methods

--- app/src/main/java/com/elynx/pogoxmitm/Injector.java
+++ app/src/main/java/com/elynx/pogoxmitm/Injector.java
@@ -1,11 +1,12 @@
 package com.elynx.pogoxmitm;
 
+import java.net.HttpURLConnection;
 import java.nio.ByteBuffer;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import de.robv.android.xposed.XC_MethodHook;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
@@ -13,10 +14,40 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
  * Class that manages injection of code into target app
  */
 public class Injector implements IXposedHookLoadPackage {
-    private static Boolean busyOut = false;
-    private static Boolean busyIn = false;
+    private static String[] Methods = {"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"};
 
-    private static final String[] Methods = {"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"};
+    private static class RpcContext {
+        long threadId = 0L;
+
+        long objectId = 0L;
+        int requestId = 0;
+        String url;
+        String method;
+        String requestHeaders;
+        String responseHeaders;
+        int responseCode = 0;
+
+        public String shortDump() {
+            StringBuilder builder = new StringBuilder();
+
+            builder.append(Integer.toHexString(requestId));
+            builder.append(":");
+            builder.append(Long.toHexString(objectId));
+            builder.append(" ");
+            builder.append(method);
+            builder.append(" ");
+            builder.append(url);
+
+            return builder.toString();
+        }
+    }
+
+    private static ThreadLocal<RpcContext> rpcContext = new ThreadLocal<RpcContext>() {
+        @Override
+        protected RpcContext initialValue() {
+            return new RpcContext();
+        }
+    };
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.nianticlabs.pokemongo"))
@@ -24,45 +55,47 @@ public class Injector implements IXposedHookLoadPackage {
 
         XposedBridge.log("Injecting into PoGo");
 
+        // method is executed in thread context
         findAndHookMethod("com.nianticlabs.nia.network.NiaNet", lpparam.classLoader,
-         // void request   0 object id 1 id       2 url->      3 method -> 4 headers ->  5 buffer ->      6 offset -> 7 size ->
-                "request", long.class, int.class, String.class, int.class, String.class, ByteBuffer.class, int.class, int.class,
+                //               0 object id 1 id       2 url         3 method   4 headers     5 buffer          6 offset   7 size
+                "doSyncRequest", long.class, int.class, String.class, int.class, String.class, ByteBuffer.class, int.class, int.class,
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        synchronized (busyOut) {
-                            busyOut = true;
+                        RpcContext context = rpcContext.get();
 
-                            //byteBuffer = (ByteBuffer)param.args[5];
-                            //byte[] bytes = {};
-                            //byteBuffer.get(bytes, param.args[6], param.args[7]);
+                        context.threadId = Thread.currentThread().getId();
 
-                            String dump = "[request " + param.args[0].toString() +  "] " + Methods[(int) param.args[3]] + " url " + (String) param.args[2] + " hdr " + (String) param.args[4];
-                            XposedBridge.log(dump);
+                        context.objectId = (long) param.args[0];
+                        context.requestId = (int) param.args[1];
+                        context.url = (String) param.args[2];
+                        context.method = Methods[(int) param.args[3]];
+                        context.requestHeaders = (String) param.args[4];
 
-                            busyOut = false;
-                        }
+                        XposedBridge.log("[request] " + context.shortDump());
                     }
                 });
 
         findAndHookMethod("com.nianticlabs.nia.network.NiaNet", lpparam.classLoader,
-         // void nativeCallback   0 object    id 1 http  2 headers     3 buffer          4 offset    5 size
-                "nativeCallback", long.class, int.class, String.class, ByteBuffer.class, int.class, int.class,
+                "joinHeaders", HttpURLConnection.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        RpcContext context = rpcContext.get();
+
+                        context.responseHeaders = (String) param.getResult();
+                        context.responseCode = ((HttpURLConnection) param.args[0]).getResponseCode();
+                    }
+                });
+
+        findAndHookMethod("com.nianticlabs.nia.network.NiaNet", lpparam.classLoader,
+                "readDataSteam", HttpURLConnection.class,
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        synchronized (busyIn) {
-                            busyIn = true;
+                        RpcContext context = rpcContext.get();
 
-                            //byteBuffer = (ByteBuffer)param.args[3];
-                            //byte[] bytes = {};
-                            //byteBuffer.get(bytes, param.args[4], param.args[5]);
-
-                            String dump = "[response " + param.args[0].toString() + "] hdr " + (String) param.args[2];
-                            XposedBridge.log(dump);
-
-                            busyIn = false;
-                        }
+                        XposedBridge.log("[response] " + context.shortDump());
                     }
                 });
     }
