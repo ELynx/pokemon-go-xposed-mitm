90f2df71bdf791b4b6011ede7b151540ae04e47e
Peter Lauck <griest024@gmail.com>
2016-08-30 21:25:54 -0400

    don't use EntryPointActivity

--- app/src/main/java/com/elynx/pogoxmitm/ScriptTestActivity.java
+++ app/src/main/java/com/elynx/pogoxmitm/ScriptTestActivity.java
@@ -1,9 +1,62 @@
 package com.elynx.pogoxmitm;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ScriptTestActivity extends org.ruboto.EntryPointActivity {
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+import org.ruboto.JRubyAdapter;
+
+public class ScriptTestActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_script_test);
+
+        Button rubyButton = (Button) findViewById(R.id.rubyRun);
+        rubyButton.setOnClickListener(this);
+
+        JRubyAdapter.setUpJRuby(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() != R.id.rubyRun)
+            return;
+
+        EditText scriptEdit = (EditText) findViewById(R.id.rubyScript);
+        EditText resultEdit = (EditText) findViewById(R.id.rubyResult);
+
+        String scriptToRun = scriptEdit.getText().toString();
+        String runResult = "Nothingness\n";
+
+        try {
+            runResult = "Trying run...\n";
+            Object rc = JRubyAdapter.runScriptlet(scriptToRun);
+
+            if (rc == null)
+                runResult += "Null returned";
+            else
+                runResult += rc.toString();
+        } catch (Throwable e) {
+            String message = e.getMessage();
+
+            if (message == null)
+                message = "No message\n";
+            else
+                message = message + "\n";
+
+            message += e.toString();
+
+            if (e.getCause() != null) {
+                message += "\nCaused by " + e.getCause().toString();
+            }
+
+            runResult += "Exception:\n" + message;
+        }
+
+        resultEdit.setText(runResult);
     }
 }
