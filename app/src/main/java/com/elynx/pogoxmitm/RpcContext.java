2093d3498a98e732ca6cdffce0b1feea0e97599b
Chris Reynoso <freedommercenary@gmail.com>
2016-09-19 22:14:35 -0700

    Add detection setting for split logic depending on whether or not doSyncRequest (PoGo < 0.37) is available to fix issue #24. When doSyncRequest does not exist, detect PoGo request data from getInputStream and getOutputStream instead and pass using existing logic for MITM.

--- app/src/main/java/com/elynx/pogoxmitm/RpcContext.java
+++ app/src/main/java/com/elynx/pogoxmitm/RpcContext.java
@@ -1,9 +1,19 @@
 package com.elynx.pogoxmitm;
 
+import java.net.HttpURLConnection;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.robv.android.xposed.XposedBridge;
+
 /**
  * Context of Rpc call in NiaNet
  */
 class RpcContext {
+    private static final String apiPrefix = "https://pgorelease.nianticlabs.com/plfe";
+    private static final String apiSuffix = "/rpc";
     long threadId = 0L;
     boolean niaRequest = false;
     boolean niaResponse = false;
@@ -15,6 +25,28 @@ class RpcContext {
     String requestHeaders;
     String responseHeaders;
     int responseCode = 0;
+
+    public static RpcContext fromConnection(URLConnection urlConnection, boolean isRequest) {
+        RpcContext rpcContext = new RpcContext();
+
+        try {
+            rpcContext.url = urlConnection.getURL().toExternalForm();
+            boolean isNia = rpcContext.url != null &&
+                    rpcContext.url.startsWith(apiPrefix) && rpcContext.url.endsWith(apiSuffix);
+            rpcContext.niaRequest = isRequest && isNia;
+            rpcContext.niaResponse = !isRequest && isNia;
+            rpcContext.threadId = Thread.currentThread().getId();
+
+            if (urlConnection instanceof HttpURLConnection) {
+                HttpURLConnection httpConnection = (HttpURLConnection) urlConnection;
+                rpcContext.method = httpConnection.getRequestMethod();
+            }
+        } catch (Throwable e) {
+            XposedBridge.log(e);
+        }
+
+        return rpcContext;
+    }
 
     public String shortDump() {
         return Integer.toString(requestId) + " " + method + " " + url;
